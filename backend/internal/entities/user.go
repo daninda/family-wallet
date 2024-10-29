4546c15717e3bfa42d1b5596bcc14ae3131bfd17
Salavat Nursafin <krowlexing@gmail.com>
2024-10-29 14:10:07 +0400

    add filter and stats entities

--- backend/internal/entities/user.go
+++ backend/internal/entities/user.go
@@ -7,5 +7,6 @@ type User struct {
 	PasswordHash string `json:"passwordHash" db:"password_hash"`
 	HouseholdId int `json:"householdId" db:"household_id"`
 	Accepted bool `json:"accepted" db:"accepted"`
+	IsAdmin bool `json:"isAdmin" db:"is_admin"`
 	Limitation *int `json:"limitation" db:"limitation"`
 }
