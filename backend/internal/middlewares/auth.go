de0fad9e31077e7979eaf44bf773e5ce10eb61f5
Salavat Nursafin <krowlexing@gmail.com>
2024-12-05 15:30:32 +0400

    add auth middleware

---
+++ backend/internal/middlewares/auth.go
@@ -0,0 +1,35 @@
+package middlewares
+
+import (
+	"context"
+	"family-wallet/internal/services"
+	"net/http"
+)
+
+type Auth struct {
+	jwtService *services.Jwt
+}
+
+type key string
+
+const User_id key = "user_id"
+
+func NewAuthMiddleware(jwtService *services.Jwt) *Auth {
+	return &Auth{jwtService}
+}
+
+func (middleware *Auth) Middleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := w.Header().Get("Authorization")
+		
+		id, err := middleware.jwtService.ValidateToken(token)
+
+		if err != nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), User_id, id)
+		handler.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
