445b7efbf52ddab671041aa7207a6b8f71f2614c
Salavat Nursafin <krowlexing@gmail.com>
2024-12-27 04:55:57 +0400

    add stats

--- frontend/src/pages/statistics/Statistics.tsx
+++ frontend/src/pages/statistics/Statistics.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Wrapper } from '../../components/wrappers/Wrapper';
 import Button from '../../components/buttons/Button';
 import { useNavigate } from 'react-router-dom';
@@ -6,19 +6,12 @@ import styled from '@emotion/styled';
 import CategorySelector from '../../components/categorySelector/CategorySelector';
 import Input from '../../components/inputs/Input';
 import Dropdown from '../../components/dropdowns/Dropdown';
-import {
-    CartesianGrid,
-    Cell,
-    Legend,
-    Line,
-    LineChart,
-    Pie,
-    PieChart,
-    ResponsiveContainer,
-    Tooltip,
-    XAxis,
-    YAxis,
-} from 'recharts';
+import { LineChart, PieChart } from '@mui/x-charts';
+import { network } from '../../services/network/network';
+
+import { Record as RecordX } from '../../models/records';
+import { Category } from '../../models/categories';
+import { MenuItem, Select } from '@mui/material';
 
 const PageWrapper = styled.div`
     padding-top: 38px;
@@ -92,6 +85,14 @@ const FilterItemInput = styled.div`
 const Statistics: FC = () => {
     const navigate = useNavigate();
 
+    const [records, setRecords] = useState<RecordX[]>([]);
+    const [categoriesRequest, setCategoriesRequest] = useState<Category[]>([]);
+
+    const [chart, setChart] = useState(0);
+
+    useEffect(() => {
+        network.category.getAll().then(setCategoriesRequest);
+    }, []);
     const categories = [
         {
             id: 1,
@@ -113,6 +114,83 @@ const Statistics: FC = () => {
         { name: 'Page E', value: 1111, uv: 1890, pv: 4800, amt: 2181 },
     ];
 
+    useEffect(() => {
+        network.record.getAll({}).then(setRecords);
+    }, []);
+
+    const [showGood, setShowGood] = useState(false);
+
+    useEffect(() => {
+        if (showGood) {
+            const timeout = setTimeout(() => setShowGood(false), 5000);
+            return () => clearTimeout(timeout);
+        }
+    }, [showGood]);
+
+    const recordsForCategory: Record<number, RecordX[]> = {};
+
+    for (const record of records) {
+        if (!recordsForCategory[record.categoryId]) {
+            recordsForCategory[record.categoryId] = [];
+        }
+        recordsForCategory[record.categoryId].push(record);
+    }
+
+    if (records.length === 0) {
+        return (
+            <Wrapper>
+                <PageWrapper>
+                    <Title>Статистика</Title>
+                    <Tools>
+                        <Button
+                            color="white"
+                            height="large"
+                            width="medium"
+                            title="Назад"
+                            onClick={() => navigate('/')}
+                        />
+                    </Tools>
+                    <MainWrapper>
+                        <StatisticsWrapper>
+                            <h1>Записей нет</h1>
+                        </StatisticsWrapper>
+                    </MainWrapper>
+                </PageWrapper>
+            </Wrapper>
+        );
+    }
+
+    const allDays = days(records);
+
+    const categoriesT = [];
+    const recordsT = [];
+    for (const [categoryId, records] of Object.entries(recordsForCategory)) {
+        const groupedDays = groupByDay(records, allDays).map((day) => {
+            return day.reduce((acc, record) => {
+                return acc + record.price;
+            }, 0);
+        });
+
+        recordsT.push(groupedDays);
+        categoriesT.push(+categoryId);
+    }
+
+    const categorySum = Object.values(recordsForCategory).map((records) => {
+        return records.reduce((acc, record) => {
+            return acc + record.price;
+        }, 0);
+    });
+
+    const categoryName = (id: number): string => {
+        console.log(id);
+        return categoriesRequest.find((c) => c.id === id)?.name ?? 'def';
+    };
+
+    const series = recordsT.map((r, i) => ({
+        data: r,
+        label: categoryName(categoriesT[i]),
+    }));
+
     return (
         <Wrapper>
             <PageWrapper>
@@ -129,51 +207,43 @@ const Statistics: FC = () => {
                 </Tools>
                 <MainWrapper>
                     <StatisticsWrapper>
-                        <ResponsiveContainer width="100%" height={400}>
-                            {statisticsType.id === 2 ? (
-                                <PieChart>
-                                    <Pie
-                                        data={data}
-                                        dataKey="value"
-                                        nameKey="name"
-                                        cx="50%"
-                                        cy="50%"
-                                        outerRadius={150}
-                                        fill="#8884d8"
-                                        label
-                                    >
-                                        {data.map((_, index) => (
-                                            <Cell
-                                                key={`cell-${index}`}
-                                                fill={
-                                                    index % 2 === 0
-                                                        ? '#8884d8'
-                                                        : '#82ca9d'
-                                                }
-                                            />
-                                        ))}
-                                    </Pie>
-                                </PieChart>
-                            ) : (
-                                <LineChart data={data}>
-                                    <CartesianGrid strokeDasharray="3 3" />
-                                    <XAxis dataKey="name" />
-                                    <YAxis />
-                                    <Tooltip />
-                                    <Legend />
-                                    <Line
-                                        type="monotone"
-                                        dataKey="uv"
-                                        stroke="#8884d8"
-                                    />
-                                    <Line
-                                        type="monotone"
-                                        dataKey="pv"
-                                        stroke="#82ca9d"
-                                    />
-                                </LineChart>
-                            )}
-                        </ResponsiveContainer>
+                        {chart === 1 ? (
+                            <PieChart
+                                series={[
+                                    {
+                                        data: categorySum.map((s, i) => ({
+                                            value: s,
+                                            label: categoryName(categoriesT[i]),
+                                        })),
+                                    },
+                                ]}
+                            ></PieChart>
+                        ) : (
+                            <LineChart
+                                series={series}
+                                yAxis={[
+                                    {
+                                        label: 'Сумма, р',
+                                    },
+                                ]}
+                                xAxis={[
+                                    {
+                                        data: allDays.map((d) => new Date(+d)),
+                                        valueFormatter: (d) =>
+                                            new Date(+d).toLocaleDateString(
+                                                'ru-RU'
+                                            ),
+                                        label: 'Дата',
+                                    },
+                                ]}
+                                //     allDays.map((d) => ({
+                                //     data: allDays,
+                                //     label: new Date(+d).toLocaleDateString(
+                                //         'ru-RU'
+                                //     ),
+                                // }))}
+                            ></LineChart>
+                        )}
                     </StatisticsWrapper>
                     <FiltersWrapper>
                         <FiltersTitle>Фильтры</FiltersTitle>
@@ -192,16 +262,16 @@ const Statistics: FC = () => {
                                     Вид статистики
                                 </FilterItemTitle>
                                 <FilterItemInput>
-                                    <Dropdown
-                                        onSelect={(item) =>
-                                            setStatisticsType(item)
+                                    <Select
+                                        defaultValue={0}
+                                        sx={{ flex: 1 }}
+                                        onChange={(e) =>
+                                            setChart(+e.target.value)
                                         }
-                                        width="wide"
-                                        items={[
-                                            { id: 1, name: 'График' },
-                                            { id: 2, name: 'Диаграмма' },
-                                        ]}
-                                    />
+                                    >
+                                        <MenuItem value={0}>График</MenuItem>
+                                        <MenuItem value={1}>Диаграмма</MenuItem>
+                                    </Select>
                                 </FilterItemInput>
                             </FilterItem>
                         </Filters>
@@ -211,5 +281,17 @@ const Statistics: FC = () => {
         </Wrapper>
     );
 };
+
+function days(records: RecordX[]): string[] {
+    const daysA = records.map((r) => r.date);
+
+    const daysB = Array.from(new Set(daysA));
+    daysB.sort();
+    return daysB;
+}
+
+function groupByDay(records: RecordX[], days: string[]): RecordX[][] {
+    return days.map((day) => records.filter((r) => r.date === day));
+}
 
 export default Statistics;
